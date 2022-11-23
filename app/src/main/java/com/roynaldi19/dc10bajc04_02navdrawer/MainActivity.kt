dcd16a76b54275b3e2f34cdd38dd03325562215b
roynaldi19 <roynaldi19@gmail.com>
2022-11-23 15:55:15 +0700

    comit awal

--- app/src/main/java/com/roynaldi19/dc10bajc04_02navdrawer/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/dc10bajc04_02navdrawer/MainActivity.kt
@@ -3,14 +3,21 @@ package com.roynaldi19.dc10bajc04_02navdrawer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.roynaldi19.dc10bajc04_02navdrawer.ui.theme.DC10BAJC04_02NavDrawerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +39,53 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyNavDrawerApp() {
+    val scaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
+    Scaffold(
+        topBar = {
+            MyTopBar(
+                onMenuClick = {
+                    scope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+
+                }
+            )
+        },
+        drawerContent = { Text(stringResource(R.string.hello_from_nav_drawer))
+        },
+        drawerGesturesEnabled =  scaffoldState.drawerState.isOpen
+    ){ paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(stringResource(R.string.hello_world))
+        }
+    }
+}
+
+@Composable
+fun MyTopBar(onMenuClick: () -> Unit) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = {
+                onMenuClick()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(R.string.menu)
+                )
+            }
+        },
+        title = {
+            Text(stringResource(R.string.app_name))
+        },
+    )
 }
 
 @Preview(showBackground = true)
